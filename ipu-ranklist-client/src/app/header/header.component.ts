4c44e3ebf45170e80cac5126b61467fdff47b67f
ankushgarg1998 <1998anky@gmail.com>
2019-07-22 00:12:25 +0530

    Options now working with NgFor

--- ipu-ranklist-client/src/app/header/header.component.ts
+++ ipu-ranklist-client/src/app/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import * as allCourses from '../shared/courses.json';
+import * as allInstis from '../shared/institutes.json';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+    courses = [];
+    instis = [];
     defaultSelection = {
         college: 'MSIT',
         shift: 'M',
@@ -20,6 +23,8 @@ export class HeaderComponent implements OnInit {
     constructor() { }
     
     ngOnInit() {
+        this.courses = allCourses['default'];
+        this.instis = allInstis['default'];
         this.ngForm.form.valueChanges.subscribe(x => {
             console.log(x);
             this.selectionsChanged.emit(x);

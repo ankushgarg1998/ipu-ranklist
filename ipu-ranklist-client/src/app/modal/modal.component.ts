0b264b0bf637f30369f99709019582ba5e17d9d2
ankushgarg1998 <1998anky@gmail.com>
2019-07-28 16:13:04 +0530

    Subject Names, Search Box, Styles

--- ipu-ranklist-client/src/app/modal/modal.component.ts
+++ ipu-ranklist-client/src/app/modal/modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ListService } from '../shared/list.service';
 
+import * as allSubjects from '../shared/subjects.json';
+
 @Component({
     selector: 'app-modal',
     templateUrl: './modal.component.html',
@@ -9,6 +11,7 @@ import { ListService } from '../shared/list.service';
 export class ModalComponent implements OnInit {
     banda = {};
     showMinorMajor = false;
+    allTheSubjects = allSubjects;
     @ViewChild('modalButton') modalButton: ElementRef;
     
     constructor(private listService: ListService) { }
@@ -17,6 +20,9 @@ export class ModalComponent implements OnInit {
         this.listService.rowSelectedEvent
         .subscribe(banda => {
             this.modalButton.nativeElement.click();
+            banda.semesters[0].subjects.map(subj => {
+                subj.name = this.allTheSubjects['default'][subj.paper_id] || subj.paper_id;
+            });
             console.log(banda);
             this.banda = banda;
         });
--- ipu-ranklist-client/src/app/table/table.component.ts
+++ ipu-ranklist-client/src/app/table/table.component.ts
@@ -9,7 +9,8 @@ import { ListService } from '../shared/list.service';
     styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit, OnChanges {
-    list= [];
+    fullList= [];
+    list = [];
     @Input() selections = {};
     
     constructor(private modelService: ModelService, private listService: ListService) { }
@@ -23,12 +24,18 @@ export class TableComponent implements OnInit, OnChanges {
             if(keys.includes('college') && keys.includes('shift') && keys.includes('batch') && keys.includes('branch') && keys.includes('sem')) {
                 this.modelService.getList(this.selections['college'], this.selections['shift'], this.selections['batch'], this.selections['branch'], this.selections['sem'])
                     .subscribe((res: any[]) => {
-                        // console.log(res);
-                        this.list = res.sort((a, b) => {
+                        console.log(res);
+                        this.fullList = res.sort((a, b) => {
                             let aMarks = a.semesters[0].total_marks;
                             let bMarks = b.semesters[0].total_marks;
                             return (aMarks < bMarks? 1: (aMarks > bMarks? -1: 0));
                         });
+                        let rank = 1;
+                        // for(let i=0; i<this.fullList.length; i++) {
+                        //     if(i>0 && this.fullList[i-1].
+                        //     this.fullList[i].rank = rank;
+                        // }
+                        this.list = this.fullList.slice();
                     });
             }
         }
@@ -37,5 +44,10 @@ export class TableComponent implements OnInit, OnChanges {
     rowClicked(index) {
         this.list[index]['rank'] = index;
         this.listService.rowSelected(this.list[index]);
+    }
+
+    onSearch(e) {
+        let str = e.srcElement.value.toUpperCase();
+        this.list = this.fullList.filter(banda => (banda.name.includes(str) || banda.enroll_no.includes(str)));
     }
 }

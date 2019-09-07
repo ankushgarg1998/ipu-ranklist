0f8c36a5e2da046709244fbc18b8a1069cd873f8
ankushgarg1998 <1998anky@gmail.com>
2019-09-07 13:12:26 +0530

    Animations Randomize added

--- ipu-ranklist-client/src/app/student/student.component.ts
+++ ipu-ranklist-client/src/app/student/student.component.ts
@@ -9,10 +9,16 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class StudentComponent implements OnInit {
 
     public lottieConfig: Object;
+    animJsons = [
+        'batman',
+        'flying-cat',
+        // 'karam'
+    ];
+    animIndex = Math.floor((Math.random() * (this.animJsons.length)));
 
     constructor(private router: Router, private route: ActivatedRoute) {
         this.lottieConfig = {
-            path: 'assets/anims/profile_anim.json',
+            path: `assets/anims/${this.animJsons[this.animIndex]}.json`,
             renderer: 'svg',
             autoplay: true,
             loop: true

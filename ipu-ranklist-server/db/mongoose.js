3d2ca0f904d7f794e6cfb719f05aef02e05ca59e
Ankush Garg <1998anky@gmail.com>
2020-01-18 13:27:40 +0530

    #23 and minor bug fixes

--- ipu-ranklist-server/db/mongoose.js
+++ ipu-ranklist-server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 // mongoose.set('debug', true);
 

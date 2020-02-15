61384c0cb50a88b6eef7283f43551daa313fe99b
ankushgarg1998 <1998anky@gmail.com>
2020-02-15 10:58:16 +0530

    Added ads.txt

--- ipu-ranklist-client/build-client.sh
+++ ipu-ranklist-client/build-client.sh
@@ -1,1 +1,4 @@
 ng build --prod --aot --output-path="../ipu-ranklist-server/dist/"
+cd ../ipu-ranklist-server/dist
+echo 'google.com, pub-2511778193246891, DIRECT, f08c47fec0942fa0' > ads.txt
+cd ../

467e965cf1fd53f44631937d413317715d977ae0
ankushgarg1998 <1998anky@gmail.com>
2020-02-15 10:58:16 +0530

    Added ads.txt

--- ipu-ranklist-client/publish-dev.sh
+++ ipu-ranklist-client/publish-dev.sh
@@ -1,5 +1,6 @@
-ng build --prod --aot
-cd dist/ipu-ranklist
-cp index.html 200.html
-echo 'ipuranklist.surge.sh' > CNAME
-surge
+# ng build --prod --aot
+# cd dist/ipu-ranklist
+# cp index.html 200.html
+# echo 'ipuranklist.surge.sh' > CNAME
+# echo 'google.com, pub-2511778193246891, DIRECT, f08c47fec0942fa0' > ads.txt
+# surge

542d87db614aa15b779bf40cebf23aaa9bf57cde
Ankush Garg <1998anky@gmail.com>
2020-03-14 19:13:28 +0530

    subject-highest tag, bvcoe issue fixed

--- ipu-ranklist-client/src/app/ranklist/list.service.ts
+++ ipu-ranklist-client/src/app/ranklist/list.service.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from '@angular/core';
 export class ListService {
     rowSelectedEvent = new EventEmitter<any>();
 
-    rowSelected(banda) {
-        this.rowSelectedEvent.emit(banda);
+    rowSelected(modalData) {
+        this.rowSelectedEvent.emit(modalData);
     }
 }
